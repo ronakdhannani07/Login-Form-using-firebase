b5c5a4f038a0c7430ffa1d854126e0c1984d6ae1
ronakdhannani07 <72298437+ronakdhannani07@users.noreply.github.com>
2020-12-26 02:04:13 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Login-Form-using-firebase
-login form for car racing game
+# synchronousBallMovement
+Ball moving synchronously
---
+++ sketch.js
@@ -0,0 +1,26 @@
+var database;
+var gameState = 0;
+var playerCount;
+var form, game, player;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+ 
+  
+   
+}
+
+
+
